a4f6b70bc1e4c350dbb180018ef61e573d4c0c4d
Paul Verity <paul.verity@v-home.co.uk>
2018-05-25 22:42:53 +0100

    reigate-junior-joggers-#5: Add editable schedules and dev server proxy config

--- src/site-constants.js
+++ src/site-constants.js
@@ -1,11 +1,10 @@
-
 const INDEX_TITLE = 'Reigate Junior Joggers';
 const INDEX_META_DESCRIPTION = 'Home of Junior Run Club in Reigate, Surrey';
 
 const FACEBOOK_URL = '/reigatejuniorjoggers';
 const TWITTER_URL = '/@juniorjoggers';
 const SITE_EMAIL_ADDRESS = 'admin@reigatejuniorjoggers.co.uk';
-const SITE_API_URL = 'http://www.reigatejuniorjoggers.co.uk/enquiries';
+const SITE_API_URL = '/enquiries';
 
 module.exports = {
 	INDEX_TITLE,
