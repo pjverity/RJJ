d241da70fc93fcb212a7fb529b591d652af5e529
Paul Verity <paul.verity@v-home.co.uk>
2019-04-09 22:50:19 +0100

    Prevent border from displaying on stacked views

--- src/components/schedules/StaticSchedule.jsx
+++ src/components/schedules/StaticSchedule.jsx
@@ -6,11 +6,15 @@ export default class StaticSchedule extends Component {
 
 		return (
 			<div className='row m-4 align-items-end text-center'>
-				<div className="col">
+				<div className="col d-block d-sm-none">
 					<p className='display-4'>Thursdays 5-6pm</p>
 					<p className="text-muted">Starting Thursday 25th April</p>
 				</div>
-				<div className="col border-left">
+				<div className="col d-none d-sm-block border-right">
+					<p className='display-4'>Thursdays 5-6pm</p>
+					<p className="text-muted">Starting Thursday 25th April</p>
+				</div>
+				<div className="col ">
 					<p className='display-4'>£5</p>
 					<p className="text-muted">Per session bookable in half term blocks</p>
 				</div>
